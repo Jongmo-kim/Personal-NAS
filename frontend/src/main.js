af63259b9f8cde63968eb49d5c29e0c8a82b6cab
Jongmo-kim <wa1358@naver.com>
2022-06-24 00:11:33 +0900

    primevue UI component 적용 #24

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from "@/router"
+import InputText from 'primevue/inputtext';
+import Button from 'primevue/button';
+import Toast from 'primevue/toast';
+import ToastService from 'primevue/toastservice';
+import 'primevue/resources/primevue.min.css';
+import 'primeicons/primeicons.css';
+import 'primevue/resources/themes/arya-blue/theme.css';
+
+Vue.use(ToastService);
+
+Vue.component('p-input', InputText);
+Vue.component('p-button', Button);
+Vue.component('p-toast', Toast);
 
 
 
